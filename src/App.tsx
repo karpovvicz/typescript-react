878255cb2a54664c4e75e5134209dd503b39bd89
karpovvicz <karpovvicz@gmail.com>
2025-07-01 11:47:16 +0200

    added ContextProvider API

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,22 @@
 import React from 'react';
-import { useState } from 'react';
+
 
 import Todos from "./components/Todos";
-import Todo from './models/todo'
+
 import NewTodo from "./components/NewTodo";
+import TodosContexProvider from "./store/todos-context";
 
 
 
 function App() {
-    const [todos, setTodos] = useState<Todo[]>([]);
 
-
-    const addTodoHandler = (todoText: string) => {
-const newTodo = new Todo(todoText);
-
-setTodos((prevTodos) => {
-    return prevTodos.concat(newTodo)
-})
-    }
 
   return (
-    <div>
-        <NewTodo onAddTodo={addTodoHandler}/>
-      <Todos items={todos} />
+    <TodosContexProvider>
+        <NewTodo/>
+      <Todos  />
 
-    </div>
+    </TodosContexProvider>
   );
 }
 
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,7 +1,10 @@
-import { useRef } from 'react';
+import { useRef, useContext } from 'react';
 import classes from './NewTodo.module.css'
+import { TodosContext} from "../store/todos-context";
 
-const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+const NewTodo: React.FC = () => {
+    const todosCtx = useContext(TodosContext);
+
     const todoTextInputRef = useRef<HTMLInputElement>(null);
 
     const submitHandler = (event: React.FormEvent) => {
@@ -16,8 +19,8 @@ if (enteredText.trim() === '') {
 }
 
 
-props.onAddTodo(enteredText);
 
+todosCtx.addTodo(enteredText)
 
     }
 
